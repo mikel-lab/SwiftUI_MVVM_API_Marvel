fdcf29588e508dc9e0d565076deb2769bf45a8ff
Mikel Cobian <mikelcobian@gmail.com>
2022-03-20 23:31:06 +0100

    actualización final antes de entrega

--- PokedexHeroes/Shared/ViewModels/PhotoViewModel.swift
+++ PokedexHeroes/Shared/ViewModels/PhotoViewModel.swift
@@ -5,4 +5,42 @@
 //  Created by Mikel Cobian on 19/3/22.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+
+final class PhotoViewModel : ObservableObject {
+    @Published var photo:Image?
+
+    var suscriptor = Set<AnyCancellable>()
+
+    func loadImage(url:String){
+  
+        //control de cache.
+        
+        // descarga de la imagen de Red
+        let urlDownload = URL(string: url)!
+        
+        URLSession.shared
+            .dataTaskPublisher(for: urlDownload)
+            .map{
+                UIImage(data: $0.data) //UIKIT
+            }
+            .map{ image -> Image in //SwiftUI
+                if let imagen = image {
+                    return Image(uiImage: imagen)
+                }
+                else {
+                    return Image(systemName: "person.fill")
+                }
+            }
+            .replaceError(with: Image(systemName: "person.fill"))
+            .replaceEmpty(with: Image(systemName: "person.fill"))
+            .receive(on: DispatchQueue.main)
+            .sink{
+                self.photo = $0
+            }
+            .store(in: &suscriptor)
+    }
+
+}
--- PokedexHeroes/Shared/Views/Heroes/HeroesRowView.swift
+++ PokedexHeroes/Shared/Views/Heroes/HeroesRowView.swift
@@ -8,13 +8,49 @@
 import SwiftUI
 
 struct HeroesRowView: View {
+    var hero: Result
+    @StateObject private var viewmodel = PhotoViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            
+            if let photo = viewmodel.photo {
+                photo
+                    .resizable()
+                    .cornerRadius(10)
+                    .aspectRatio(contentMode: .fit)
+                    .padding([.leading, .trailing], 20)
+                    .opacity(0.6)
+            } else{
+                Image(systemName: "photo")
+                    .resizable()
+                    .cornerRadius(10)
+                    .aspectRatio(contentMode: .fit)
+                    .padding([.leading, .trailing], 20)
+                    .opacity(0.6)
+            }
+            
+            Text(if let name = hero.name {
+                .font(.title2)
+                .foregroundColor(.gray)
+                .bold()
+            }else{
+                name ="Heroe random"
+                    .font(.title2)
+                    .foregroundColor(.gray)
+                    .bold()
+            } )
+                
+        }
+        .onAppear{
+            viewmodel.loadImage(url: "\(hero.thumbnail.getUrlImage(photoType.portrait))")
+        }
     }
 }
 
 struct HeroesRowView_Previews: PreviewProvider {
     static var previews: some View {
-        HeroesRowView()
+        HeroesRowView(hero: viewModelHeros(testing: true).getHeroUIDesing())
+            .previewLayout(.fixed(Width: 350, heigth: 350))
     }
 }
